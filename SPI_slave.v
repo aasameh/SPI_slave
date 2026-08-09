8ff01b55cacb218cfc64bf7d5079897a3e7420e2
aasameh <ahmedsameh99187@gmail.com>
2026-08-09 20:30:59 +0300

    Revert "fix SIPO register reset"
    This reverts commit 6a7eac68678dc3ce7a134f7dbeb2f428449b45ee.

--- SPI_slave.v
+++ SPI_slave.v
@@ -9,7 +9,7 @@ module SPI_slave #(
     input [TX_SIZE-1:0] tx_data,
     input tx_valid,
     input read_addr_received,
-    output MISO,
+    output reg MISO,
     output reg [RX_SIZE-1:0] rx_data,
     output reg rx_valid
 );
@@ -18,106 +18,116 @@ module SPI_slave #(
         input integer value;
         integer i;
         begin
-            value = value - 1;
-            for (i = 0; value > 0; i = i + 1) value = value >> 1;
+            value = value -1;
+            for(i=0; value>0; i=i+1) value = value>>1;
             clog2 = i;
         end
     endfunction
 
-    localparam IDLE      = 3'b000;
-    localparam CHK_CMD   = 3'b001;
-    localparam WRITE     = 3'b010;
+    localparam IDLE = 3'b000;
+    localparam CHK_CMD = 3'b001;
+    localparam WRITE = 3'b010;
     localparam READ_DATA = 3'b011;
-    localparam READ_ADD  = 3'b100;
-
-    localparam SP_CNT_WIDTH = clog2(RX_SIZE + 1);
+    localparam READ_ADD = 3'b100;
+    localparam SP_CNT_WIDTH = clog2(RX_SIZE);
     localparam PS_CNT_WIDTH = clog2(TX_SIZE);
 
-    reg [2:0] cs, ns;
+    reg [2:0] cs;
+    reg [2:0] ns;
     reg [SP_CNT_WIDTH-1:0] sp_counter;
     reg [PS_CNT_WIDTH-1:0] ps_counter;
-    
     wire load;
     reg piso_loaded;
     wire piso_en;
     wire sipo_en;
-    wire [RX_SIZE-1:0] sipo_output;
-
+    reg [RX_SIZE-1:0] SIPO_register;
+    reg [TX_SIZE-1:0] PISO_register;
 
     //state memory block
     always @(posedge clk) begin
-        if (~rst) begin
-            cs <= IDLE;
-        end else begin
-            cs <= ns;
+        if(~rst) begin
+            cs<=IDLE;
         end
+        else cs<=ns;
     end
 
-    // Next state logic block
+    //next state logic block
     always @(*) begin
         ns = cs;
         case (cs)
             IDLE: begin
-                if (SS_n) ns = IDLE;
+                if(SS_n) ns = IDLE;
                 else ns = CHK_CMD;
             end
 
             CHK_CMD: begin
-                if (SS_n) ns = IDLE;
+                if(SS_n) ns = IDLE;
                 else begin
-                    if (~MOSI) ns = WRITE;
+                    if(~MOSI) ns = WRITE;
                     else if (MOSI && ~read_addr_received) ns = READ_ADD;
                     else if (MOSI && read_addr_received) ns = READ_DATA;
                 end
             end
 
-            WRITE:     ns = (SS_n) ? IDLE : WRITE;
-            READ_ADD:  ns = (SS_n) ? IDLE : READ_ADD;
+            WRITE: ns = (SS_n) ? IDLE : WRITE;
+
+            READ_ADD: ns = (SS_n) ? IDLE : READ_ADD;
+
             READ_DATA: ns = (SS_n) ? IDLE : READ_DATA;
 
             default: ns = IDLE;
         endcase
     end
     
-assign sipo_en = ~SS_n && (cs == WRITE || cs == READ_ADD || cs == READ_DATA);
+assign sipo_en = ~SS_n && (cs != IDLE);
 assign piso_en = ~SS_n && (cs == READ_DATA) && piso_loaded && (ps_counter < TX_SIZE - 1);
 
-    // SUBMODULE INSTANTIATIONS
-    SIPO #(.DATA_WIDTH(RX_SIZE)) sipo (
-        .clk(clk),
-        .rst(rst),
-        .SI(MOSI),
-        .shift_en(sipo_en),
-        .PO(sipo_output)
-    );
 
     // SIPO #(.DATA_WIDTH(RX_SIZE)) sipo(
     //     .clk(clk),
     //     .rst(rst),
     //     .SI(MOSI),
     //     .shift_en(sipo_en),
-    //     .PO(sipo_output)
+    //     .PO(SIPO_register)
     // );
 
-    // Counters
+    // PISO #(.DATA_WIDTH(TX_SIZE)) piso(
+    //     .clk(clk),
+    //     .rst(rst),
+    //     .load(load),
+    //     .PI(tx_data),
+    //     .shift_en(piso_en),
+    //     .SO(MISO)
+    // );
+
+    //counters
     always @(posedge clk) begin
-        if(~rst || cs == IDLE) sp_counter <= 1;
+        if(~rst || cs == IDLE) sp_counter <= 0;
         else if (sipo_en) sp_counter <= sp_counter + 1'b1;
     end
 
     always @(posedge clk) begin
-        if (~rst || cs == IDLE || load) ps_counter <= 0;
+        if(~rst || cs == IDLE || load) ps_counter <= 0;
         else if (piso_en) ps_counter <= ps_counter + 1'b1;
     end
 
+    assign load = tx_valid && (cs == READ_DATA);
+
     always @(posedge clk) begin
-        if (~rst || cs != READ_DATA)
+        if(~rst || cs != READ_DATA)
             piso_loaded <= 1'b0;
-        else if (load)
+        else if(load)
             piso_loaded <= 1'b1;
     end
+    //output logic block
+    //internal signals: 
+    //sp counter increments change with every clock cycle after chk_cmd
+    //read_received asserts if din[9:8] == 2'b10
+    //MISO changes with every clock cycle
+    //rx_data is the full output of SIPO after sp counter equals
 
-    // Output logic block
+
+        //SIPO
     always @(posedge clk) begin
         if(~rst) SIPO_register <= {RX_SIZE{1'b0}};
         else if (sipo_en) SIPO_register <= {SIPO_register[RX_SIZE-2:0], MOSI};
@@ -130,14 +140,14 @@ assign piso_en = ~SS_n && (cs == READ_DATA) && piso_loaded && (ps_counter < TX_S
     always @(posedge clk) begin
         if(~rst) begin
             PISO_register <= {TX_SIZE{1'b0}};
-            SO <= 1'b0;
+            MISO <= 1'b0;
         end
         else if(load) begin
-            SO <= PI[TX_SIZE-1];
-            PISO_register <= {PI[TX_SIZE-2:0], 1'b0};
+            MISO <= tx_data[TX_SIZE-1];
+            PISO_register <= {tx_data[TX_SIZE-2:0], 1'b0};
         end
-        else if(shift_en) begin
-            SO <= PISO_register[TX_SIZE-1];
+        else if(piso_en) begin
+            MISO <= PISO_register[TX_SIZE-1];
             PISO_register <= {PISO_register[TX_SIZE-2:0], 1'b0};
         end
     end
@@ -148,7 +158,7 @@ assign piso_en = ~SS_n && (cs == READ_DATA) && piso_loaded && (ps_counter < TX_S
             rx_data <= {RX_SIZE{1'b0}};
             rx_valid <= 0;
         end else begin
-            rx_valid <= 1'b0;
+            rx_valid <= 0;
             case (cs)
                 IDLE: begin
                     SIPO_register <= 0;
@@ -159,7 +169,7 @@ assign piso_en = ~SS_n && (cs == READ_DATA) && piso_loaded && (ps_counter < TX_S
             //rx_data is also one cycle old so we reconstruct the next value
                 WRITE: begin
                     if(sp_counter == RX_SIZE-1) begin
-                        rx_data <= {sipo_output[RX_SIZE-2:0], MOSI};
+                        rx_data <= {SIPO_register[RX_SIZE-2:0], MOSI};
                         rx_valid <= 1'b1;
                     end
                     //write addr vs write data logic is in RAM module side depending on the first two bits
@@ -167,14 +177,14 @@ assign piso_en = ~SS_n && (cs == READ_DATA) && piso_loaded && (ps_counter < TX_S
 
                 READ_ADD: begin
                     if(sp_counter == RX_SIZE-1) begin
-                        rx_data <= {sipo_output[RX_SIZE-2:0], MOSI};
+                        rx_data <= {SIPO_register[RX_SIZE-2:0], MOSI};
                         rx_valid <= 1'b1;
                     end
                 end
 
                 READ_DATA: begin
                     if(sp_counter == RX_SIZE-1) begin
-                        rx_data <= {sipo_output[RX_SIZE-2:0], MOSI};
+                        rx_data <= {SIPO_register[RX_SIZE-2:0], MOSI};
                         rx_valid <= 1'b1;
                     end
 
@@ -188,26 +198,20 @@ assign piso_en = ~SS_n && (cs == READ_DATA) && piso_loaded && (ps_counter < TX_S
 
 endmodule
 
-// Shift-left SIPO Register
+//shift-left SIPO register
 module SIPO #(
-    parameter DATA_WIDTH = 10
+    parameter DATA_WIDTH = 8
 ) (
     input clk,
     input rst,
     input SI,
     input shift_en,
-    output reg [DATA_WIDTH-1:0] PO
+    output [DATA_WIDTH-1:0] PO
 );
-    always @(posedge clk) begin
-        if (~rst) begin
-            PO <= {DATA_WIDTH{1'b0}};
-        end else if (shift_en) begin
-            PO <= {PO[DATA_WIDTH-2:0], SI};
-        end
-    end
+
 endmodule
 
-// Shift-left PISO Register
+// shift-left PISO register
 module PISO #(
     parameter DATA_WIDTH = 8
 ) (
@@ -218,18 +222,8 @@ module PISO #(
     input shift_en,
     output reg SO
 );
-    reg [DATA_WIDTH-1:0] register;
 
-    always @(posedge clk) begin
-        if (~rst) begin
-            register <= {DATA_WIDTH{1'b0}};
-            SO       <= 1'b0;
-        end else if (load) begin
-            SO       <= PI[DATA_WIDTH-1];
-            register <= {PI[DATA_WIDTH-2:0], 1'b0};
-        end else if (shift_en) begin
-            SO       <= register[DATA_WIDTH-1];
-            register <= {register[DATA_WIDTH-2:0], 1'b0};
-        end
-    end
+reg [DATA_WIDTH-1:0] register;
+
+
 endmodule
